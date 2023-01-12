88803d4e66f8fc25c4b36dca7f022c4c23ed7a71
Ruan <ruan.amaral.lemos@gmail.com>
2023-01-11 21:28:06 -0300

    backEnd funcionando 100%

--- BackEnd/src/controllers/PokeController.js
+++ BackEnd/src/controllers/PokeController.js
@@ -1,12 +1,19 @@
-// import axios from "axios";
-// const pokeApi = `https://pokeapi.co/api/v2/`;
+import axios from "axios";
+const pokeApi = `https://pokeapi.co/api/v2/`;
 
-// class PokeController{
+class PokeController {
+  async index(req, res) {
+    const url = `${pokeApi}pokemon`;
+    const response = await axios.get(url);
+    return res.json(response.data);
+  }
 
-//     async index(req,res){
-//         const url
+  async show(req, res) {
+    const { index } = req.body;
+    const url = `${pokeApi}pokemon/${index}/`;
+    const response = await axios.get(url);
+    return res.json(response.data)
+  }
+}
 
-
-//     }
-
-// }
+export default new PokeController();
--- BackEnd/src/routes.js
+++ BackEnd/src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import SessionControler from "./controllers/SessionController";
+import PokeController from "./controllers/PokeController";
 
 const routes = new Router();
 
 routes.post("/sessionsCreate", SessionControler.store);
 
 routes.get("/sessionsStart", SessionControler.show);
+
+routes.get("/pokemon", PokeController.index);
+
+routes.get("/pokemon/info", PokeController.show);
+
 
 export default routes;
